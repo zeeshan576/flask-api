c604e93a502f02fccd4a33cb971c2274839b3bc0
zeeshan576 <shanhadi513@gmail.com>
2026-05-14 00:31:18 +0500

    add flask-cors

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import pickle
 import numpy as np
 import os
 
 # 1. Yahan double underscore aayega
-app = Flask(__name__) 
+app = Flask(__name__)
+CORS(app) 
 
 with open("model.pkl", "rb") as file:
     model = pickle.load(file)
